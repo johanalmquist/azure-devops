e6c784902fa41e2d170abf47ed005881e4541212
Johan Almquist <johan.almquist@aranya.se>
2023-05-08 16:58:49 +0200

    update some prod stuff

---
+++ terraform/prod/main.tf
@@ -0,0 +1,40 @@
+#####################################
+# SET UP TERRAFORM PROVIDERS TO USE #
+#####################################
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.24.0" # Telling terraform to use the exat version 3.24.0 of AzureRM
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.3.0"
+    }
+
+  }
+  backend "azurerm" {
+    resource_group_name  = "ara-terraform"
+    storage_account_name = "araterraform"
+    container_name       = "services-tfstate"
+    key                  = "johan-testing-staging.tfstate"
+  }
+}
+provider "azurerm" {}
+
+data "http" "example" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+output "latest_version" {
+  value = jsondecode(data.http.example.response_body)
+}
+
+output "respone_code" {
+  value = data.http.example.status_code
+}
