09208a5fce41a62fb53dcf22bcb3cad6b45ea8eb
Johan Almquist <johan.almquist@aranya.se>
2023-05-11 10:23:58 +0200

    update terraform

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -36,7 +36,7 @@ output "latest_version" {
   value = jsondecode(data.http.example.response_body)
 }
 
-output "respone_codes" {
+output "respone_code" {
   value = data.http.example.status_code
 }
 
